fe9b2a85f157475a8ac320894ddedabc76288896
maxespejojr <maximoespejojr@gmail.com>
2025-10-20 19:00:59 +0800

    HelloWorldAppcommit1

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,7 +1,8 @@
 public class HelloWorldApp {
 
     public static void main(String[] args) {
-        System.out.println("Kamusta Mundo" +
-                "");
+        System.out.println("Kamusta Mundo");
+        System.out.println("Kamusta Mundo");
     }
+
 }
